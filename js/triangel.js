41650c6833d1c529634cd58e54ce58f2721762f8
mkurzyk <37752301+mkurzyk@users.noreply.github.com>
2018-04-06 15:54:09 +0200

    corr

--- js/triangel.js
+++ js/triangel.js
@@ -1,11 +1,11 @@
 
 var a = 2;
 var h = 5;
-triangleArea = a*h/2;
+var triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
 /* with prompt */
-var a = prompt("Triangle field with base: ","");
-var h = prompt("Height: ","");
-triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var a1 = prompt("Triangle field with base: ","");
+var h1 = prompt("Height: ","");
+var triangleArea = a1*h1/2;
+console.log('Triangle field with base a: ' + a1 + ' and height h: ' + h1 + ' is equal to: ' + triangleArea);
