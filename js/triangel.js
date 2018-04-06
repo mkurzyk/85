4a7ca16aac932f2be77b95222dc1530525bc1421
Mariusz Kurzyk <mariusz.kurzyk@gmail.com>
2018-04-06 08:11:54 +0200

    triangle field js added

---
+++ js/triangel.js
@@ -0,0 +1,11 @@
+
+var a = 2;
+var h = 5;
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+/* with prompt */
+var a = prompt("Triangle field with base: ","");
+var h = prompt("Height: ","");
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
